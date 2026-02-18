7fef9ad038b6421da575c030336c480621ac5d91
Rodrigo Polydoro <rspolydoro@gmail.com>
2026-02-18 17:42:15 -0300

    fix: google auth

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@
  */
 export const environment = {
   production: true,
-  apiUrl: 'https://estoque-central-api.onrender.com' // Update with your actual Render URL
+  apiUrl: 'https://estoque-central-api.onrender.com', // Update with your actual Render URL
+  googleClientId: '153827874012-hpucuakdod6diu9ipuisqq4lhl8r9ovr.apps.googleusercontent.com'
 };
